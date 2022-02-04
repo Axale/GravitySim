c72b6d0b650e9d94b5a1f3675e0da3019eb17b76
Will Hagan <38008781+Axale@users.noreply.github.com>
2022-02-04 02:22:00 -0500

    Project Finish
    For now, all the important parts of this project is finished. I will probably go in and comment later, as well as do some optimization.
    This commit splits the 3 body simulation into two files (while preserving the original). This is done so that the visual representation of the simulation can be sped up, as the simulation uses small timesteps.
    Be warned, the log files take up a fair amount of space (100 MB)

---
+++ orbitsimshow.py
@@ -0,0 +1,39 @@
+from time import sleep
+import pygame as game
+import sys as sys
+        
+
+def main():
+    game.init()
+    black = game.Color(0, 0, 0)
+    white = game.Color(255, 255, 255)
+
+    displayscreen = game.display.set_mode((1200, 800))
+    displayscreen.fill(black)
+    logs = open('GravitySim/logs.txt', 'r')
+    inputlogs = logs.read()
+    inputlist = inputlogs.split(" ")
+    index = 0
+    lenlist = len(inputlist)
+
+    while 1:
+        
+        for event in game.event.get():
+            if event.type == game.QUIT: sys.exit()
+        if index >= lenlist:
+            break
+        
+        print(index)
+        displayscreen.fill(black)
+        Draw(displayscreen, white, float(inputlist[index]), float(inputlist[index+1]))
+        Draw(displayscreen, white, float(inputlist[index + 2]), float(inputlist[index + 3]))
+        Draw(displayscreen, white, float(inputlist[index + 4]), float(inputlist[index + 5]))
+        game.display.flip()
+        index += 30
+
+def Draw(displayscreen, white, Coord1, Coord2):
+    if Coord1 > 0 and Coord1 < 1200 and Coord2 > 0 and Coord2 < 800:
+        game.draw.circle(displayscreen, white, game.Vector2(Coord1, Coord2), 5, width=0)
+
+if __name__ == "__main__":
+    main()
---
+++ orbitsimtostore.py
@@ -0,0 +1,64 @@
+import pygame as game
+
+timestep = .01
+
+
+class MassObject:
+
+    def __init__(self, location, velocity, mass):
+        self.location = game.Vector2(location)
+        self.white = game.Color(255, 255, 255)
+        self.velocity = game.Vector2(velocity)
+        self.mass = mass
+
+    def RelVects(self, Onex, Oney, Twox, Twoy):
+        self.relvectr2 = game.Vector2((self.location.x - Onex, self.location.y - Oney))
+        self.relvectr3 = game.Vector2(((self.location.x - Twox, self.location.y - Twoy)))
+
+    def Move(self):
+        self.location.x += self.velocity.x * timestep + .5 * self.accelx * (timestep ** 2)
+        self.location.y += self.velocity.y * timestep + .5 * self.accely * (timestep ** 2)
+        self.velocity.x += self.accelx * timestep
+        self.velocity.y += self.accely * timestep
+
+    def CalcMove(self, MassOne, MassTwo):
+
+        self.accelx = -MassOne * self.relvectr2.x/((self.relvectr2.magnitude())**(3)) - MassTwo * self.relvectr3.x/((self.relvectr3.x**2 + self.relvectr3.y**2)**(3/2))
+        self.accely = -MassOne * self.relvectr2.y/((self.relvectr2.magnitude())**(3)) - MassTwo * self.relvectr3.y/((self.relvectr3.x**2 + self.relvectr3.y**2)**(3/2))
+
+
+def main():
+    Fixed = MassObject((550.69873, 375), ((3**.5)/2, -1/2), 80)
+    Moving = MassObject((643.30127, 375), ((3**.5)/2, 1/2), 80)
+    Third = MassObject((600, 450), (-1, 0), 80)
+    logs = open('GravitySim/logs.txt', 'w')
+    totaltime = 0
+
+    while 1:
+        output = str((Fixed.location.x, Fixed.location.y, Moving.location.x, Moving.location.y, Third.location.x, Third.location.y))
+        output = output.replace(',', '')
+        output = output.replace('(', '')
+        output = output.replace(')', '')
+        output += " "
+
+        logs.write(output)
+
+        Moving.RelVects(Fixed.location.x, Fixed.location.y, Third.location.x, Third.location.y)
+        Fixed.RelVects(Moving.location.x, Moving.location.y, Third.location.x, Third.location.y)
+        Third.RelVects(Moving.location.x, Moving.location.y, Fixed.location.x, Fixed.location.y)
+
+        Fixed.CalcMove(Moving.mass, Third.mass)
+        Moving.CalcMove(Fixed.mass, Third.mass)
+        Third.CalcMove(Moving.mass, Fixed.mass)
+
+        Moving.Move()
+        Fixed.Move()
+        Third.Move()
+        totaltime += timestep
+        if totaltime >= 10000.02:
+            logs.close()
+            exit()
+
+
+if __name__ == "__main__":
+    main()
